d3cbfb7b2b3847cc97754191efd676583ee3c90a
castillocrx <105761899+castillocrx@users.noreply.github.com>
2024-09-21 15:22:17 -0600

    Agregar archivos de proyecto.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaContabilidadAltosDelAbejonal
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
