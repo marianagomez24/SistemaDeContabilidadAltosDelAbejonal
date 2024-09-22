52e90f86968362a06a597c75a2535f22ba5df7d6
castillocrx <105761899+castillocrx@users.noreply.github.com>
2024-09-22 16:36:21 -0600

    Plantilla implementada

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -11,20 +11,32 @@ namespace SistemaContabilidadAltosDelAbejonal
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //// Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
+            //// para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new Bundle("~/bundles/js").Include(
+                      "~/assets/js/pages/layout.js",
+                       "~/assets/libs/jquery/jquery.min.js",
+                        "~/assets/libs/bootstrap/js/bootstrap.bundle.min.js",
+                         "~/assets/libs/metismenu/metisMenu.min.js",
+                          "~/assets/libs/simplebar/simplebar.min.js",
+                          "~/assets/libs/node-waves/waves.min.js",
+                          "~/assets/libs/parsleyjs/parsley.min.js",
+                          "~/assets/js/pages/form-validation.init.js",
+                          "~/assets/js/app.js",
+                          "~/assets/libs/apexcharts/apexcharts.min.js",
+                          "~/assets/js/pages/dashboard.init.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/assets/css/bootstrap.min.css",
+                      "~/assets/css/icons.min.css",
+                      "~/assets/libs/simplebar/simplebar.min.css",
+                      "~/assets/css/app.min.css"));
         }
     }
 }
