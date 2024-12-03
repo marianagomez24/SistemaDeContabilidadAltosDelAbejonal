da8efa0bd0a9cc44370d551e8a8d056c6d814546
castillocrx <105761899+castillocrx@users.noreply.github.com>
2024-12-02 19:17:15 -0600

    Cotizaciones  (#10)
    * Crear cotizaciones
    
    * Vistas arregladas cotizaciones
    
    * Cotizaciones (falta borrado logico)

---
+++ Controllers/CotizacionesController.cs
@@ -0,0 +1,115 @@
+﻿using SistemaContabilidadAltosDelAbejonal.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaContabilidadAltosDelAbejonal.Controllers
+{
+    public class CotizacionesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CotizacionesController()
+        {
+
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var productos = _context.Productos.Select(p => new ProductoSeleccionado
+            {
+                IDProducto = p.IDProducto,
+                NombreProducto = p.Nombre,
+                PrecioUnitario = p.Precio,
+                Stock = p.Stock
+            }).ToList();
+
+            var viewModel = new CotizacionViewModel
+            {
+                ProductosSeleccionados = productos
+            };
+
+            ViewBag.Clientes = _context.Cliente
+                .Select(c => new { c.IDCliente, NombreCompleto = c.Nombre + " " + c.PrimerApellido })
+                .ToList();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CotizacionViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Clientes = _context.Cliente
+                    .Select(c => new { c.IDCliente, NombreCompleto = c.Nombre + " " + c.PrimerApellido })
+                    .ToList();
+                return View(viewModel);
+            }
+
+            var cotizacion = new Cotizacion
+            {
+                IDCliente = viewModel.IDCliente,
+                FechaCotizacion = DateTime.Now,
+                Estado = "Pendiente",
+                TotalSinIVA = viewModel.ProductosSeleccionados.Sum(p => p.Cantidad * p.PrecioUnitario),
+                IVA = viewModel.ProductosSeleccionados.Sum(p => p.Cantidad * p.PrecioUnitario) * 0.16m,
+                TotalConIVA = viewModel.ProductosSeleccionados.Sum(p => p.Cantidad * p.PrecioUnitario * 1.16m),
+                Observaciones = viewModel.Observaciones
+            };
+
+            _context.Cotizaciones.Add(cotizacion);
+            _context.SaveChanges();
+
+            foreach (var producto in viewModel.ProductosSeleccionados.Where(p => p.Cantidad > 0))
+            {
+                var detalle = new CotizacionDetalle
+                {
+                    IDCotizacion = cotizacion.IDCotizacion,
+                    IDProducto = producto.IDProducto,
+                    Cantidad = producto.Cantidad,
+                    PrecioUnitario = producto.PrecioUnitario
+                };
+
+                _context.CotizacionDetalles.Add(detalle);
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Create");
+        }
+
+        public ActionResult Index()
+        {
+            var cotizaciones = _context.Cotizaciones.ToList();
+            var clientes = _context.Cliente.ToList();
+            ViewBag.Clientes = clientes;
+            return View(cotizaciones);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var cotizacion = _context.Cotizaciones
+                .Where(c => c.IDCotizacion == id)
+                .FirstOrDefault();
+
+            if (cotizacion == null)
+            {
+                return HttpNotFound();
+            }
+
+            var cotizacionDetalles = _context.CotizacionDetalles
+                .Where(cd => cd.IDCotizacion == id)
+                .Include(cd => cd.Producto) 
+                .ToList();
+
+            ViewBag.Cotizacion = cotizacion;
+            return View(cotizacionDetalles);
+        }
+    }
+}
---
+++ Models/CotizacionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaContabilidadAltosDelAbejonal.Models
+{
+    public class CotizacionViewModel
+    {
+        public int IDCliente { get; set; } 
+        public List<ProductoSeleccionado> ProductosSeleccionados { get; set; } = new List<ProductoSeleccionado>();
+        public string Observaciones { get; set; } 
+    }
+
+    public class ProductoSeleccionado
+    {
+        public int IDProducto { get; set; }
+        public string NombreProducto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
