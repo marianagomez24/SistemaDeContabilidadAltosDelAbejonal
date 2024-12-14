a7c8c53c6f262ff13e69d45e4afe76ea657f43cd
Ari0812 <107007639+Ari8012@users.noreply.github.com>
2024-12-13 20:49:37 -0600

    Cambio configuracion de inicio.

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace SistemaContabilidadAltosDelAbejonal
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "IniciarSesion", id = UrlParameter.Optional }
             );
         }
     }
